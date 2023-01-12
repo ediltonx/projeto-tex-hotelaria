98d7569221c948c86bfa4bb1306b6efa6b3c1250
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-01-12 20:31:22 -0300

    Atualização final valida formulário

--- public/assets/js/validaForm.js
+++ public/assets/js/validaForm.js
@@ -2,22 +2,22 @@ const password = "123456";
 const email = "edmar.ade@gmail.com";
 
 function validarFormulario() {
-  var login = document.querySelector("#login").value;
-  let senha = document.querySelector("#senha").value;
-  login = login.replace(/"|\s|'/g, "").trim();
+    var login = document.querySelector("#login").value;
+    let senha = document.querySelector("#senha").value;
+    login = login.replace(/"|\s|'/g, "").trim();
 
-  if (!login.length == 0 && !senha.length == 0) {
-    if (login == email && senha == password) {
-      localStorage.clear()
-      let key = 'Email'
-      localStorage.setItem(key, login)
-      return true
+    if (!login.length == 0 && !senha.length == 0) {
+        if (login == email && senha == password) {
+            localStorage.clear();
+            let key = "Email";
+            localStorage.setItem(key, login);
+            return true;
+        } else {
+            alert("Verificar login/senha");
+            return false;
+        }
     } else {
-      alert("Verificar login/senha");
-      return false;
+        alert("Preencher todos os campos.");
+        return false;
     }
-  } else {
-    alert("Preencher todos os campos.");
-    return false;
-  }
 }
